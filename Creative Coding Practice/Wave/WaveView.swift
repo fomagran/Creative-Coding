1f52e4548295d0f973d76ca5f4fed242e803f389
Fomagran <fomagran6@naver.com>
2021-12-05 20:54:22 +0900

    [add] Fomagran 투명한 글자에 파도치는거 보이게 하기

---
+++ Creative Coding Practice/Wave/WaveView.swift
@@ -0,0 +1,101 @@
+//
+//  WaveView.swift
+//  Creative Coding Practice
+//
+//  Created by Fomagran on 2021/12/05.
+//
+
+import UIKit
+
+class WaveView: UIView {
+    
+    private var timer : Timer?
+    private var points:[Point] = []
+    private var h:CGFloat = 0
+    private var lineCount:Int = 3
+    private var dotCount:Int = 4
+    private let colors:[UIColor] = [.systemPink,.systemBlue,.systemCyan,.systemMint,.systemTeal]
+    private var lines:[Line] = []
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        let w:CGFloat = self.bounds.width/CGFloat(dotCount+1)
+        let center = self.frame.height/2
+        h = self.frame.height/2
+        for i in 1...dotCount {
+            let x:CGFloat = w*CGFloat(i)
+            let y:CGFloat = center
+            let max:CGFloat = CGFloat.random(in: h/2...h)
+            let min:CGFloat = CGFloat.random(in: (-h)...(-h/2))
+            let addY:CGFloat = CGFloat(i%2) == 0 ? -1:1
+            points.append(Point(x: x, y: y, max: max, min: min, addY: addY))
+        }
+        startTimer()
+    }
+    
+    private func appendLines() {
+        for _ in 0..<lineCount {
+            lines.append(Line(layer:CAShapeLayer(),p1:(0..<dotCount).randomElement()!, p2:(0..<dotCount).randomElement()!, color: (colors.randomElement()!)))
+        }
+    }
+    
+    private func addCurve(line:Line,p1:Int,p2:Int) {
+        line.layer.removeFromSuperlayer()
+        let height:CGFloat = self.frame.height
+        let width:CGFloat = self.frame.width
+        let linePath = UIBezierPath()
+        linePath.move(to:CGPoint(x: 0, y:height))
+        linePath.addLine(to:CGPoint(x: 0, y:height/2))
+        linePath.addCurve(to:CGPoint(x: width, y:height/2), controlPoint1:points[p1].center, controlPoint2:points[p2].center)
+        linePath.addLine(to: CGPoint(x: width, y: height))
+        line.layer.path = linePath.cgPath
+        line.layer.fillColor = line.color.withAlphaComponent(0.4).cgColor
+        layer.addSublayer(line.layer)
+        
+    }
+    
+    //MARK:- @objc Functions
+    
+    @objc func timerCallback() {
+        for i in 0..<points.count {
+            points[i].y += points[i].addY
+            if points[i].y > h + points[i].max  || points[i].y < h + points[i].min {
+                points[i].addY = -(points[i].addY)
+            }
+        }
+        
+        for i in 0..<lineCount {
+            addCurve(line:lines[i],p1:lines[i].p1,p2:lines[i].p2)
+        }
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerCallback), userInfo: nil, repeats: true)
+        for line in lines {
+            line.layer.fillColor = UIColor.clear.cgColor
+        }
+        lines.removeAll()
+        appendLines()
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func handleTimer() {
+        if timer == nil {
+            startTimer()
+        }else {
+            stopTimer()
+        }
+    }
+}
--- Creative Coding Practice/Wave/WaveViewController.swift
+++ Creative Coding Practice/Wave/WaveViewController.swift
@@ -9,10 +9,29 @@ import UIKit
 
 class WaveViewController: UIViewController {
     
+    var w:WaveView!
     
     override func viewDidLoad() {
+        self.view.backgroundColor = UIColor(displayP3Red: 55/255, green: 55/255, blue: 55/255, alpha: 1)
         super.viewDidLoad()
-        let w = WaveView(frame:CGRect(x:0, y: self.view.center.y, width: self.view.frame.width, height: 200))
+        w = WaveView(frame:CGRect(x:0, y: self.view.center.y-200, width: self.view.frame.width, height: 400))
+        
         self.view.addSubview(w)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        w.addGestureRecognizer(tapGesture)
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x:self.view.center.x-100, y: self.view.center.y-100, width: 200, height: 200)
+        imageView.image = UIImage(named: "fomagran.png")
+        imageView.contentMode = .scaleToFill
+        imageView.layer.cornerRadius = 100
+        imageView.layer.masksToBounds = true
+        self.view.addSubview(imageView)
     }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        w.handleTimer()
+    }
+    
+    
 }

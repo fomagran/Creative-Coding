74ccbacd89030a8cbe9db9d2412ba62e15ed5eeb
Fomagran <fomagran6@naver.com>
2021-12-22 20:23:03 +0900

    [DoctorStrange] CircleView 리팩터링

--- Creative Coding Practice/DoctorStrange/View/CircleView.swift
+++ Creative Coding Practice/DoctorStrange/View/CircleView.swift
@@ -14,44 +14,23 @@ class CircleView:UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.clear
-        
-        // Use UIBezierPath as an easy way to create the CGPath for the layer.
-        // The path should be the entire circle.
         let circlePath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: (frame.size.width - 10)/2, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2.0), clockwise: true)
-        
-        // Setup the CAShapeLayer with the path, colors, and line width
         circleLayer = CAShapeLayer()
         circleLayer.path = circlePath.cgPath
         circleLayer.fillColor = UIColor.clear.cgColor
         circleLayer.strokeColor = UIColor.lightGray.cgColor
         circleLayer.lineWidth = 2.0;
-        
-        // Don't draw the circle initially
         circleLayer.strokeEnd = 0.0
-        
-        // Add the circleLayer to the view's layer's sublayers
         layer.addSublayer(circleLayer)
     }
     
     func animateCircle(duration: TimeInterval) {
-        // We want to animate the strokeEnd property of the circleLayer
         let animation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeEnd))
-
-        // Set the animation duration appropriately
         animation.duration = duration
-
-        // Animate from 0 (no circle) to 1 (full circle)
         animation.fromValue = 0
         animation.toValue = 1
-
-        // Do a linear animation (i.e. the speed of the animation stays the same)
         animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-
-        // Set the circleLayer's strokeEnd property to 1.0 now so that it's the
-        // right value when the animation ends.
         circleLayer.strokeEnd = 1.0
-
-        // Do the actual animation
         circleLayer.add(animation, forKey: "animateCircle")
     }
     

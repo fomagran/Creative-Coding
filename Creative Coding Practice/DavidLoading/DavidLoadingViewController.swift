85d49016a72a4d348a7305b2469707d186dd5b05
Fomagran <fomagran6@naver.com>
2021-12-23 22:54:57 +0900

    [DavidLoading] 분홍벽,다비드 45도 기울게 하고 초기 위치에 세팅하기

--- Creative Coding Practice/DavidLoading/DavidLoadingViewController.swift
+++ Creative Coding Practice/DavidLoading/DavidLoadingViewController.swift
@@ -11,7 +11,17 @@ class DavidLoadingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        let pinkwall:UIImageView = UIImageView(image: UIImage(named:"pinkwall.png"))
+        let david:UIImageView = UIImageView(image: UIImage(named:"david.png"))
+        pinkwall.frame = CGRect(x: view.frame.midX-200, y: view.frame.midY-200, width: 400, height: 400)
+        view.addSubview(pinkwall)
+        david.frame = CGRect(x: view.frame.midX-50, y: view.frame.midY-50, width: 100, height: 100)
+        view.addSubview(david)
+        pinkwall.transform = pinkwall.transform.rotated(by: -.pi/4)
+        david.transform = david.transform.rotated(by: .pi/4)
+        let centerY = pinkwall.center.y
+        let dot = centerY - (400*sqrt(2)/2)
+        david.center = CGPoint(x: view.frame.midX+70, y: dot)
+        
     }
 }

6c4d1c92d86178d5dfe56f878af94f142d3b07ff
Fomagran <fomagran6@naver.com>
2022-01-08 12:47:55 +0900

    [LP] LP판 가운데에 색넣기

--- Creative Coding Practice/LP/LPViewController.swift
+++ Creative Coding Practice/LP/LPViewController.swift
@@ -9,26 +9,33 @@ import UIKit
 
 class LPViewController: UIViewController {
     
-    let radius:[CGFloat] = [150,140,130,120,110,100,90,80,70,60,50]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        
+        drawLP()
+    }
+    
+    func drawLP() {
+        let radius:[CGFloat] = [150,140,130,120,110,100,90,80,70,60,50]
         for i in 0..<radius.count {
             if i == 0 {
-                drawCircle(radius[i], 0.0)
+                drawCircle(radius[i], 0.0,.black)
                 continue
             }
-            drawCircle(radius[i], 0.5)
+            if i == radius.count-1 {
+                drawCircle(radius[i], 0.5, .systemBlue)
+                continue
+            }
+            drawCircle(radius[i], 0.5,.black)
         }
     }
     
-    func drawCircle(_ radius:CGFloat,_ lineWidth:CGFloat) {
+    func drawCircle(_ radius:CGFloat,_ lineWidth:CGFloat,_ fillColor:UIColor) {
         let circleLayer = CAShapeLayer()
         let circlePath = UIBezierPath(arcCenter: CGPoint(x: view.bounds.midX, y: view.bounds.midY), radius:radius, startAngle: 0.0, endAngle: CGFloat(Double.pi * 2.0), clockwise: true)
         circleLayer.path = circlePath.cgPath
-        circleLayer.fillColor = UIColor.black.cgColor
+        circleLayer.fillColor = fillColor.cgColor
         circleLayer.strokeColor = UIColor.lightGray.cgColor
         circleLayer.lineWidth = lineWidth
         circleLayer.strokeEnd = 1.0

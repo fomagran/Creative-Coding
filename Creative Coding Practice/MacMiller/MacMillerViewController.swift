ba2ea8303aa04a8051af11e2cca5ca5a1d503658
Fomagran <fomagran6@naver.com>
2022-01-21 13:07:23 +0900

    [MacMiller] 드래그하면 따라오게끔 구현

--- Creative Coding Practice/MacMiller/MacMillerViewController.swift
+++ Creative Coding Practice/MacMiller/MacMillerViewController.swift
@@ -9,7 +9,25 @@ import UIKit
 
 class MacMillerViewController: UIViewController {
     
+    // MARK: - Properties
+    
     var iv:UIImageView!
+    lazy var panGestureRecognizer:
+    UIPanGestureRecognizer = {
+        let gestureRecognizer = UIPanGestureRecognizer()
+        gestureRecognizer
+            .addTarget(self, action: #selector(handleDrag(sender:)))
+        return gestureRecognizer
+    }()
+    
+    lazy var particleEmitter: CAEmitterLayer = {
+        let emitter = CAEmitterLayer()
+        emitter.emitterShape = .point
+        emitter.renderMode = .additive
+        return emitter
+    }()
+    
+    let starParticle = Particle()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,20 +36,43 @@ class MacMillerViewController: UIViewController {
         iv.center = view.center
         view.addSubview(iv)
         view.backgroundColor = .systemRed
-        animation()
-        
+        iv.isUserInteractionEnabled = true
+//        springAnimation()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handTap))
+        iv.addGestureRecognizer(tap)
+        iv.addGestureRecognizer(panGestureRecognizer)
     }
     
-    func animation() {
-        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options:[]) {
+    func springAnimation() {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity:0.1, options:[]) {
             self.iv.layer.transform = CATransform3DMakeScale(1.5,1.5,1)
         } completion: { _ in
-            UIView.animate(withDuration: 1) {
+            UIView.animate(withDuration: 0.3) {
                 self.iv.layer.transform = CATransform3DMakeScale(1,1,1)
-            } completion: { _ in
-                self.animation()
             }
         }
+    }
+    
+    // MARK: - Actions
+    
+    func showStars() {
+        particleEmitter.emitterCells = [starParticle]
+        self.view.layer.addSublayer(particleEmitter)
+    }
+    
+    @objc func handleDrag(sender: UIPanGestureRecognizer) {
+        iv.center = sender.location(in: self.view)
+        particleEmitter.emitterPosition = sender.location(in: self.view)
+        if sender.state == .ended {
+            particleEmitter.lifetime = 0
+        } else if sender.state == .began {
+            showStars()
+            particleEmitter.lifetime = 1.0
+        }
+    }
+    
+    @objc func handTap(sender:UITapGestureRecognizer) {
+        springAnimation()
     }
 }
 

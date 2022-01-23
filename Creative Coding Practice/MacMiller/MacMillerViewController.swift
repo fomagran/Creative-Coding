1cb329f0d8e6341e6dd1ae7af1667d60d868545d
Fomagran <fomagran6@naver.com>
2022-01-23 17:15:24 +0900

    [MacMiller] 음악 소리 크기에 맞게 자동으로 애니메이션 되게하기

--- Creative Coding Practice/MacMiller/MacMillerViewController.swift
+++ Creative Coding Practice/MacMiller/MacMillerViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MacMillerViewController: UIViewController {
     
     // MARK: - Properties
+    
     var iv:UIImageView!
     var glitchView:GlitchView!
     let particle = Particle()
     var random:[CGFloat] = [1,0,0,1,0,1,0,1]
+    var player: AVAudioPlayer!
+    private var timer: Timer?
+    private let updateInterval = 1
+    private let animatioтDuration = 0.05
+    private let maxPowerDelta: CGFloat = 30
     
     lazy var panGestureRecognizer:
     UIPanGestureRecognizer = {
@@ -32,6 +39,7 @@ class MacMillerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setSound()
         iv = UIImageView(image: UIImage(named: "macmiller.png"))
         iv.frame = CGRect(origin: .zero, size: CGSize(width: 200, height: 250))
         iv.center = view.center
@@ -46,9 +54,10 @@ class MacMillerViewController: UIViewController {
         iv.addGestureRecognizer(tap)
         iv.addGestureRecognizer(panGestureRecognizer)
         tap.require(toFail: doubleTap)
+        timerStart()
     }
     
-    func springAnimation(gview:UIView?) {
+    func springAnimation(gview:UIView?,scale:CGFloat) {
         let v = gview == nil ? iv : gview
         UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity:0.1, options:[]) {
             v!.layer.transform = CATransform3DMakeScale(1.3,1.3,1)
@@ -59,7 +68,7 @@ class MacMillerViewController: UIViewController {
         }
     }
     
-    func showGlitch() {
+    func showGlitch(scale:CGFloat) {
         let glitchView = GlitchView(frame:CGRect(x:0, y:0, width: 200, height: 250))
         glitchView.bounds.origin = CGPoint(x: iv.center.x-100, y: iv.center.y-125)
         view.addSubview(glitchView)
@@ -67,7 +76,7 @@ class MacMillerViewController: UIViewController {
         glitchView.center = iv.center
         iv.isHidden = true
         glitchView.isHidden = false
-        springAnimation(gview: glitchView)
+        springAnimation(gview: glitchView,scale:scale)
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
             glitchView.removeFromSuperview()
             self.iv.isHidden = false
@@ -80,6 +89,39 @@ class MacMillerViewController: UIViewController {
         let random6 = 1
         random = [ CGFloat(1), CGFloat(0),
                    CGFloat(random1),CGFloat(random2),CGFloat(random3),CGFloat(random4),CGFloat(random5),CGFloat(random6)]
+    }
+    
+    func setSound() {
+        guard let url = Bundle.main.url(forResource: "Clubhouse", withExtension: "mp3") else { return }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+            player.isMeteringEnabled = true
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func timerStart() {
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(updateInterval),
+                                     target: self,
+                                     selector: #selector(self.updateMeters),
+                                     userInfo: nil,
+                                     repeats: true)
+        player.play()
+    }
+    
+    
+    
+    // Calculate average power from all channels
+    private func averagePowerFromAllChannels() -> CGFloat {
+        var power: CGFloat = 0.0
+        (0..<player.numberOfChannels).forEach { (index) in
+            power = power + CGFloat(player.averagePower(forChannel: index))
+        }
+        return power
     }
     
     // MARK: - Actions
@@ -100,13 +142,40 @@ class MacMillerViewController: UIViewController {
         }
     }
     
+    func showParticle(_ posistion:CGPoint) {
+        particleEmitter.emitterPosition = posistion
+        showStars()
+        particleEmitter.lifetime = 1.0
+    }
+    
+    //MARK:- @obfc
+    
     @objc func handTap(sender:UITapGestureRecognizer) {
-        showGlitch()
+        showGlitch(scale:1.3)
     }
     
     @objc func handDoubleTap(sender:UITapGestureRecognizer) {
-        springAnimation(gview: nil)
+        springAnimation(gview: nil,scale: 1.3)
+    }
+    
+    @objc private func updateMeters() {
+        player.updateMeters()
+        let power = averagePowerFromAllChannels()
+        let scale = (power + 20)/10
+        if scale > 0 {
+            if scale > 1.5 {
+                showGlitch(scale: scale)
+            }else {
+                particleEmitter.lifetime = 0
+                springAnimation(gview: nil, scale: scale)
+            }
+        }else {
+            let randomX = (0...400).randomElement()!
+            let randomY = (0...800).randomElement()!
+            showParticle(CGPoint(x: CGFloat(randomX), y: CGFloat(randomY)))
+        }
     }
 }
+
 
 

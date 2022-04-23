86dc615aaef8254878440bd1a0f1a84a08613bf1
Fomagran <fomagran6@naver.com>
2022-04-23 18:53:15 -0500

    [Transformer] UIViiew draw 메서드에서 init으로 변경중

--- Creative Coding Practice/Transformer/CircleProgressView.swift
+++ Creative Coding Practice/Transformer/CircleProgressView.swift
@@ -16,35 +16,47 @@ class CircularProgressView: UIView {
         }
     }
 
-    // Adjust these to meet your needs. 90 degrees is the bottom of the circle
     static let startDegrees: CGFloat = 315
     static let endDegrees: CGFloat = 225
-
-    override func draw(_ rect: CGRect) {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         let startAngle: CGFloat = radians(of: CircularProgressView.startDegrees)
         let progressAngle = radians(of: CircularProgressView.startDegrees + (360 - CircularProgressView.startDegrees + CircularProgressView.endDegrees) * CGFloat(max(0.0, min(percent, 1.0))))
 
+      
         let center = CGPoint(x: bounds.midX, y: bounds.midY)
         let radius = min(center.x, center.y) - trackBorderWidth / 2 - 20
 
+        let arcLayer:CAShapeLayer = CAShapeLayer()
         let arcPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: progressAngle, clockwise: true)
+        arcPath.close()
         arcPath.lineWidth = trackBorderWidth
         arcPath.lineCapStyle = .round
+        arcLayer.strokeColor = UIColor.clear.cgColor
+        arcLayer.path = arcPath.cgPath
+        arcLayer.fillColor = progressColor.cgColor
         
-        progressColor.set()
-        arcPath.stroke()
         
+        let straightLayer:CAShapeLayer = CAShapeLayer()
+        straightLayer.frame = frame
         let straightPath = UIBezierPath()
         straightPath.move(to: CGPoint(x: center.x, y: 20))
         straightPath.addLine(to: CGPoint(x: center.x, y:40))
+        straightPath.close()
         straightPath.lineWidth = trackBorderWidth
         straightPath.lineCapStyle = .round
-        
-        progressColor.set()
-        straightPath.stroke()
-        
+        straightLayer.path = arcPath.cgPath
+        straightLayer.strokeColor = UIColor.clear.cgColor
+        straightLayer.fillColor = progressColor.cgColor
+        layer.mask = arcLayer
+        layer.addSublayer(straightLayer)
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private func radians(of degrees: CGFloat) -> CGFloat {
         return degrees / 180 * .pi
     }

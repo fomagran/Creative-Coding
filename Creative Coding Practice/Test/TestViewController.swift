e0e705a833fedc0227324473e755421fa0ebdb87
Fomagran <fomagran6@naver.com>
2022-05-30 16:29:03 -0500

    [Test] Apply EasierPath

--- Creative Coding Practice/Test/TestViewController.swift
+++ Creative Coding Practice/Test/TestViewController.swift
@@ -6,9 +6,19 @@
 //
 
 import UIKit
+import EasierPath
 
 class TestViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let easierPath = EasierPath(100,100)
+                    .right(100)
+                    .curve(.down(200), .bezier(.rightDown(50,50), .leftDown(25,150)))
+                    .left(100)
+                    .curve(.up(200), .bezier(.rightUp(25,50), .leftUp(50,150)))
+        
+        let layer = easierPath.makeLayer(3,.white,.systemPink)
+        view.layer.addSublayer(layer)
     }
 }

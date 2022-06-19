c81b71c1b505fa5995ba2c3299e186a5563de43f
Fomagran <fomagran6@gmail.com>
2022-06-18 23:40:04 -0500

    [Water] Started Water Project

---
+++ Creative Coding Practice/Water/WaterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WaterViewController.swift
+//  Creative Coding Practice
+//
+//  Created by Fomagran on 2022/06/18.
+//
+
+import UIKit
+
+class WaterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
